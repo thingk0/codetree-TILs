b7eff3d75d5d9a54ee1c03bf24de576cca7a18f3
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-06 16:15:21 +0900

    Title: 1이 3개 이상 있는 위치, Time: 126ms, Memory: 9MB, Status: Passed - CodeTree

--- 240406/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240406/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,33 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
     static int n, result = 0;
     static int[] dr = {0, -1, 0, 1};
     static int[] dc = {1, 0, -1, 0};
-
+    static StringTokenizer st;
     public static void main(String[] args) {
-        
-        n = readInt();
-        int[][] map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i][j] = readInt();
-            }
-        }
 
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                int cnt = 0;
-                for (int i = 0; i < 4; i++) {
-                    int tr = r + dr[i];
-                    int tc = c + dc[i];
-                    if (isRange(tr, tc) && map[tr][tc] == 1) {
-                        cnt++;
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+
+            n = Integer.parseInt(br.readLine());
+            int[][] map = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    int cnt = 0;
+                    for (int i = 0; i < 4; i++) {
+                        int tr = r + dr[i];
+                        int tc = c + dc[i];
+                        if (isRange(tr, tc) && map[tr][tc] == 1) {
+                            cnt++;
+                        }
+                    }
+                    if (cnt >= 3) {
+                        result++;
                     }
                 }
-                if (cnt >= 3) {
-                    result++;
-                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         System.out.print(result);
@@ -35,17 +47,5 @@ public class Main {
 
     static boolean isRange(int r, int c) {
         return 0 <= r && r < n && 0 <= c && c < n;
-    }
-
-    static int readInt() {
-        int input = 0, output = 0;
-        try {
-            while ((input = System.in.read()) != -1 && Character.isDigit(input)) {
-                output = output * 10 + (input - '0');
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return output;
     }
 }
