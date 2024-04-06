bc6f910b6520c6a74dd39b6b7597d5c2cb22a4df
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-06 16:04:35 +0900

    Title: 1이 3개 이상 있는 위치, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240406/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    static int n, result = 0;
+    static int[] dr = {0, -1, 0, 1};
+    static int[] dc = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        
+        n = readInt();
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = readInt();
+            }
+        }
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                int cnt = 0;
+                for (int i = 0; i < 4; i++) {
+                    int tr = r + dr[i];
+                    int tc = c + dc[i];
+                    if (isRange(tr, tc) && map[tr][tc] == 1) {
+                        cnt++;
+                    }
+                }
+                if (cnt >= 3) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.print(result);
+    }
+
+    static boolean isRange(int r, int c) {
+        return 0 <= r && r < n && 0 <= c && c < n;
+    }
+
+    static int readInt() {
+        int input, output = 0;
+        try {
+            while ((input = System.in.read()) != -1 && Character.isDigit(input)) {
+                output = (output << 3) + (output << 1) + (input & 15);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+}
