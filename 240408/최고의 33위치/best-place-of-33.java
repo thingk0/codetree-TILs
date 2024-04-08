c158e2597820ceebd50613c017faf4de855ec6ed
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-08 22:01:16 +0900

    Title: 최고의 33위치, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240408/최고의 33위치/best-place-of-33.java
+++ 240408/최고의 33위치/best-place-of-33.java
@@ -6,43 +6,34 @@ public class Main {
 
     static int N, result = Integer.MIN_VALUE;
     static int[][] map;
-    static StringTokenizer st;
+    static int[][] prefixSum;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
+        prefixSum = new int[N+1][N+1];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                prefixSum[i+1][j+1] = prefixSum[i+1][j] + prefixSum[i][j+1] - prefixSum[i][j] + map[i][j];
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                result = Math.max(result, countCoinInRange(i, j));
+        for (int i = 0; i <= N-3; i++) {
+            for (int j = 0; j <= N-3; j++) {
+                int coinCount = getSum(i, j, i+3, j+3);
+                result = Math.max(result, coinCount);
             }
         }
 
         System.out.print(result);
     }
 
-    static int countCoinInRange(int r, int c) {
-        int count = 0;
-        for (int i = r; i <= r + 2; i++) {
-            for (int j = c; j <= c + 2; j++) {
-                if (inRange(i, j) && map[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-
-    static boolean inRange(int r, int c) {
-        return 0 <= r && r < N && 0 <= c && c < N;
+    static int getSum(int r1, int c1, int r2, int c2) {
+        return prefixSum[r2][c2] - prefixSum[r2][c1] - prefixSum[r1][c2] + prefixSum[r1][c1];
     }
 }
