f0b716d598b6fc1f30d0a8dbf28a6dd1d20ac212
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-08 21:36:30 +0900

    Title: 줄 세우기 2, Time: 287ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240408/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+class Student implements Comparable<Student> {
+    
+    int height;
+    int weight;
+    int num;
+
+    Student(int height, int weight, int num) {
+        this.height = height;
+        this.weight = weight;
+        this.num = num;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        if (this.height == other.height) {
+            return other.weight - this.weight;
+        }
+        return this.height - other.height;
+    }
+
+}
+
+public class Main {
+
+    static int N;
+    static Student[] students;
+    static StringTokenizer st;
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        students = new Student[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            students[i] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i + 1);
+        }
+
+        Arrays.sort(students);
+        for (Student s : students) {
+            result.append(String.format("%d %d %d\n", s.height, s.weight, s.num));
+        }
+        System.out.print(result);
+    }
+}
