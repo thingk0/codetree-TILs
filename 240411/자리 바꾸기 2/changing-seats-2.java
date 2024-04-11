0382094a28958c9824d04c9f0cc07b3768b09476
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-11 21:01:25 +0900

    Title: 자리 바꾸기 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240411/자리 바꾸기 2/changing-seats-2.java
+++ 240411/자리 바꾸기 2/changing-seats-2.java
@@ -8,9 +8,9 @@ public class Main {
 
     static int N, K;
     static StringTokenizer st;
-    static int[] arr;
-    static int[][] changeArr;
-    static ArrayList<HashSet<Integer>> list = new ArrayList<>();
+    static int[] positions;
+    static int[][] swapOperations;
+    static ArrayList<HashSet<Integer>> swapHistory = new ArrayList<>();
     static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
@@ -20,43 +20,36 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        arr = new int[N + 1];
+        positions = new int[N + 1];
         for (int i = 0; i <= N; i++) {
             HashSet<Integer> set = new HashSet<>();
             set.add(i); 
-            list.add(set);
-            arr[i] = i;
+            swapHistory.add(set);
+            positions[i] = i;
         }
 
-
-
-        changeArr = new int[K][];
+        swapOperations = new int[K][];
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            changeArr[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+            swapOperations[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
         }
 
         for (int i = 0; i < 3; i++) {
-            for (int[] ch : changeArr) {
-
+            for (int[] ch : swapOperations) {                
                 int li = ch[0];
                 int ri = ch[1];
 
-                int le = arr[li];
-                int re = arr[ri];
+                int temp = positions[li];
+                positions[li] = positions[ri];
+                positions[ri] = temp;
 
-                arr[li] = re;
-                arr[ri] = le;
-
-                list.get(le).add(ri);
-                list.get(re).add(li);
-
+                swapHistory.get(positions[li]).add(ri);
+                swapHistory.get(positions[ri]).add(li);
             }
         }
 
         for (int i = 1; i <= N; i++) {
-            HashSet<Integer> set = list.get(i);
-            result.append(set.size()).append('\n');
+            result.append(swapHistory.get(i).size()).append('\n');
         }
 
         System.out.print(result);
