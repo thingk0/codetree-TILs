a658822bb6a5d776f0390f1b9603ecb305ff709b
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-11 20:56:18 +0900

    Title: 자리 바꾸기 2, Time: 1570ms, Memory: 70MB, Status: Passed - CodeTree

---
+++ 240411/자리 바꾸기 2/changing-seats-2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+public class Main {
+
+    static int N, K;
+    static StringTokenizer st;
+    static int[] arr;
+    static int[][] changeArr;
+    static ArrayList<HashSet<Integer>> list = new ArrayList<>();
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            HashSet<Integer> set = new HashSet<>();
+            set.add(i); 
+            list.add(set);
+            arr[i] = i;
+        }
+
+
+
+        changeArr = new int[K][];
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            changeArr[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int[] ch : changeArr) {
+
+                int li = ch[0];
+                int ri = ch[1];
+
+                int le = arr[li];
+                int re = arr[ri];
+
+                arr[li] = re;
+                arr[ri] = le;
+
+                list.get(le).add(ri);
+                list.get(re).add(li);
+
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            HashSet<Integer> set = list.get(i);
+            result.append(set.size()).append('\n');
+        }
+
+        System.out.print(result);
+    }
+}
