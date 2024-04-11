d693402c7232a7b57623b6a5474950c866c46bbc
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-11 20:07:21 +0900

    Title: 데이터 비교, Time: 496ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240411/데이터 비교/data-comparison.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static StringTokenizer st;
+    static HashSet<Integer> set = new HashSet<>();
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        br.readLine();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        br.readLine();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            if (set.contains(num)) {
+                result.append("1 ");
+            } else {
+                result.append("0 ");
+            }
+        }
+
+        System.out.print(result);
+    }
+}
