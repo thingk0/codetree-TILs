b7be4335206c206ec8763b17f14711e003db8d60
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-12 12:34:54 +0900

    Title: 그래프 탐색, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240412/그래프 탐색/graph-traversal.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+
+public class Main {
+
+    static int N, M;
+    static int[] parent;
+    static int[] rank;
+    static StringTokenizer st;
+
+    static int find(int num) {
+        if (num == parent[num]) {
+            return num;
+        }
+
+        return parent[num] = find(parent[num]);
+    }
+
+    static boolean union(int a, int b) {
+        
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if (aRoot == bRoot) {
+            return false;
+        }
+
+        if (rank[aRoot] <= rank[bRoot]) {
+            parent[aRoot] = bRoot;
+            if (rank[aRoot] == rank[bRoot]) {
+                rank[bRoot]++;
+            }
+        } else {
+            parent[bRoot] = parent[aRoot];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        rank = new int[N + 1];
+        parent = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : parent) {
+            set.add(num);
+        }
+
+        System.out.print(set.size() - 1);
+    }
+}
