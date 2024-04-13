c8b6939066955da2ed88562d1187fcb79f499f16
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-13 23:17:00 +0900

    Title: xor 결과 최대 만들기, Time: 114ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240413/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, result = 0;
+    static int[] nums;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        comb(new int[M], 0, 0);
+        System.out.print(result);
+    }
+
+    static void comb(int[] selected, int level, int start) {
+        if (level == M) {
+            int xor = 0;
+            for (int i : selected) {
+                xor ^= nums[i];
+            }
+            result = Math.max(result, xor);
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            selected[level] = i;
+            comb(selected, level + 1, i + 1);
+        }
+    }
+}
