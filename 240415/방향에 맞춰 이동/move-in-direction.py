38497372eddb24a403437047577b472e13a0e61f
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-15 13:21:00 +0900

    Title: 방향에 맞춰 이동, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240415/방향에 맞춰 이동/move-in-direction.py
+++ 240415/방향에 맞춰 이동/move-in-direction.py
@@ -1,16 +1,21 @@
+directions = {
+    'N': (0, 1),
+    'S': (0, -1),
+    'E': (1, 0),
+    'W': (-1, 0)
+}
+
+def update_coordinates(coordinates, direction, distance):
+    if direction in directions:
+        dx, dy = directions[direction]
+        coordinates[0] += dx * distance
+        coordinates[1] += dy * distance
+
 N = int(input())
+coordinates = [0, 0]
 
-coordinate = [0, 0]
+for _ in range(N):
+    direction, distance = input().split()
+    update_coordinates(coordinates, direction, int(distance))
 
-for i in range(N):
-    dir, dis = input().split()
-    if dir == 'N':
-        coordinate[1] += int(dis)
-    elif dir == 'S':
-        coordinate[1] -= int(dis)
-    elif dir == 'E':
-        coordinate[0] += int(dis)
-    elif dir == 'W':
-        coordinate[0] -= int(dis)
-
-print(coordinate[0], coordinate[1])
+print(coordinates[0], coordinates[1])
