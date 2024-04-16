95c0e7ba5bb2472aac875b092d4d95a0f0fdb85f
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-16 15:48:09 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 240416/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -1,7 +1,7 @@
 N, M = map(int, input().split())
 
 # 그리드 초기화
-grid = [[0] * M for _ in range(N)]
+grid = [[None] * M for _ in range(N)]
 
 # 방향
 dr = [0, 1, 0, -1]
@@ -14,17 +14,17 @@ grid[r][c] = 'A'
 def in_range(r, c):
     return 0 <= r < N and 0 <= c < M
 
-for ch in range(66, 65 + (N * M)):
+for ch in range(1, N * M):
 
     nr = r + dr[dir_num]
     nc = c + dc[dir_num]
 
-    if (not in_range(nr, nc) or grid[nr][nc] != 0):
+    if (not in_range(nr, nc) or grid[nr][nc] is not None):
         dir_num = (dir_num + 1) % 4
 
     r += dr[dir_num]
     c += dc[dir_num]
-    grid[r][c] = chr(ch)
+    grid[r][c] = chr(65 + (ch % 26))
 
 
 for row in grid:
