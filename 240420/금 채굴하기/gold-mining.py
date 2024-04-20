3bd19bea05f982e2e75ab849f893dc4d9195e041
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-20 20:35:03 +0900

    Title: 금 채굴하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240420/금 채굴하기/gold-mining.py
+++ 240420/금 채굴하기/gold-mining.py
@@ -8,35 +8,34 @@ def in_range(r, c):
 
 # k 영역일 때, 채굴 비용
 def mining_costs(k):
-    return (k ** 2) + ((k + 1) ** 2)
+    return (k * k) + ((k + 1) * (k + 1))
 
-# r,c 영역에서의 최대 금 마진
-def gold_margin(r, c):
+# r,c 영역에서의 금 카운트
+def get_gold_count(r, c, k):
 
-    max_margin = (grid[r][c] * M) - mining_costs(0)
-    prev_margin = max_margin
+    if (k == 0):
+        return grid[r][c]
 
-    for k in range(1, 2 * (N - 1) + 1):
-        gold_cnt = 0
-        nr = r - k
-        nc = c
-        for dr, dc in zip(drs, dcs):
-            for dis in range(k):
-                nr += dr
-                nc += dc
-                if (in_range(nr, nc)):
-                    gold_cnt += grid[nr][nc]
+    gold_cnt = 0
+    nr = r - k
+    nc = c
 
-        accrued_margin = prev_margin + (gold_cnt * M - mining_costs(k))
-        max_margin = max(max_margin, accrued_margin)
-        prev_margin = accrued_margin
-    
-    return max_margin
+    for dr, dc in zip(drs, dcs):
+        for dis in range(k):
+            nr += dr
+            nc += dc
+            if (in_range(nr, nc)):
+                gold_cnt += grid[nr][nc]
 
+    return gold_cnt
 
 res = 0
 for r in range(N):
     for c in range(N):
-        res = max(res, gold_margin(r, c))
+        gold_cnt = 0
+        for k in range(2 * (N - 1) + 1):
+            gold_cnt += get_gold_count(r, c, k)
+            if gold_cnt * M > mining_costs(k):
+                res = max(res, gold_cnt)
 
-print(res - 1)
+print(res)
