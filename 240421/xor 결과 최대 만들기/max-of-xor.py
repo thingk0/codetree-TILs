f9c406cd242b439050118a689833e6b773356293
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-21 14:35:14 +0900

    Title: xor 결과 최대 만들기, Time: 284ms, Memory: 28MB, Status: Passed - CodeTree

--- 240421/xor 결과 최대 만들기/max-of-xor.py
+++ 240421/xor 결과 최대 만들기/max-of-xor.py
@@ -2,21 +2,18 @@ N,M = map(int, input().split())
 arr = list(map(int, input().split()))
 
 res = 0
-selected = []
+def comb(idx, depth, accumulated_sum):
 
-def comb(idx, depth):
+    global res
     if depth == M:
-        global res
-        xor_sum = 0
-        for num in selected:
-            xor_sum ^= num
-        res = max(res, xor_sum)
+        res = max(res, accumulated_sum)
+        return
+    
+    if idx == N:
         return
 
-    for i in range(idx, N):
-        selected.append(arr[i])
-        comb(i + 1, depth + 1)
-        selected.pop()
+    comb(idx + 1, depth, accumulated_sum)
+    comb(idx + 1, depth + 1, accumulated_sum ^ arr[idx])
 
-comb(0, 0)
+comb(0, 0, 0)
 print(res)
