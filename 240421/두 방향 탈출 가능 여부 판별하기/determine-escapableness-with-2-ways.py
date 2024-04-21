c0576086812f0b5ab894dd3ab3fe46bbef488fb0
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-21 19:35:48 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5045ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240421/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240421/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,27 +1,27 @@
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
-
 drs, dcs = [1, 0], [0, 1]
+visited = set()
 
 def in_range(r, c):
-    return 0 <= r < N and 0 <= c < M 
+    return 0 <= r < N and 0 <= c < M
 
 res = 0
 def dfs(r = 0, c = 0):
     global res
+    if (r, c) in visited or grid[r][c] == 0:
+        return
 
+    if (r == N - 1) and (c == M - 1):
+        res = 1
+        return 
+
+    visited.add((r, c))
     for dr, dc in zip(drs, dcs):
         nr, nc = r + dr, c + dc
-
-        if not in_range(nr, nc) or grid[nr][nc] == 0:
-            continue
-        
-        if (nr == N - 1) and (nc == M - 1):
-            res = 1
-            return 
-
-        grid[nr][nc] = 1 
-        dfs(nr, nc)
+        if in_range(nr, nc):
+            dfs(nr, nc)
+    visited.remove((r, c))
 
 dfs()
 print(res)
