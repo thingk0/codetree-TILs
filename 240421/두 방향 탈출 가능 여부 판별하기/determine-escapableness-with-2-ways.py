18b8be4330f2ba0003738c13ce4a2a60da9bab41
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-21 19:31:10 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5040ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240421/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240421/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -7,7 +7,7 @@ def in_range(r, c):
     return 0 <= r < N and 0 <= c < M 
 
 res = 0
-def dfs(r=0, c=0):
+def dfs(r = 0, c = 0):
     global res
 
     for dr, dc in zip(drs, dcs):
@@ -16,7 +16,7 @@ def dfs(r=0, c=0):
         if not in_range(nr, nc):
             continue
         
-        if grid[nr][nc] == 1:
+        if grid[nr][nc] == 0:
             continue
 
         if (nr == N - 1) and (nc == M - 1):
