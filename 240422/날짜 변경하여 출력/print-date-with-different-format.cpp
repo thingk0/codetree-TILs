3be8da71c76d576ceb7b11858cc43564fff67e8b
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-22 23:32:26 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << b << '-' << c << '-' << a;
+    return 0;
+}
