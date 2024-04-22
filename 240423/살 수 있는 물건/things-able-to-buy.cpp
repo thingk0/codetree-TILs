a67b1249a396cdf546558774bb9b3e053ea37166
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-23 00:18:08 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240423/살 수 있는 물건/things-able-to-buy.cpp
+++ 240423/살 수 있는 물건/things-able-to-buy.cpp
@@ -8,8 +8,10 @@ int main() {
 
     if (n >= 3000) {
         cout << "book";
-    } else {
+    } else if (n >= 1000) {
         cout << "mask";
+    } else {
+        cout << "no";
     }
     return 0;
 }
