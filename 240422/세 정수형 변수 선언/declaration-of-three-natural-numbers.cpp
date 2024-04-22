47b0a481f2856c492e92807f3ce7c6c45abc58b0
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-22 12:28:41 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int a = 7;
+	int b = 23;
+	int c = 30;
+
+    cout << a << " + " << b << " = " << c;
+
+	return 0;
+
+}
