f96cee1ae27fdaa6f877325bc034043332ebccd8
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-22 23:57:57 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    int avg = sum / 3;
+
+    cout << sum << endl;
+    cout << avg << endl;
+    cout << sum - avg << endl;
+
+    return 0;
+}
