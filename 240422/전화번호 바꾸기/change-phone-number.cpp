bcd812c1f231992ecae945625f1f29ddd70c4ea9
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-22 23:35:49 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << '0' << a << '-' << c << '-' << b;
+
+    return 0;
+}
