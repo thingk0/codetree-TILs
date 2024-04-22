c951711d989be90b1e8924890f2607cc76d58986
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-22 09:25:50 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 142ms, Memory: 27MB, Status: Passed - CodeTree

--- 240422/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240422/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -13,23 +13,15 @@ vistied = [
 def in_range(r, c):
     return 0 <= r < N and 0 <= c < M
 
-
-# 아래 -> 오른쪽 : 우선순위
 drs, dcs = [1, 0], [0, 1]
-
 def move(r = 0, c = 0):
-
     for dr, dc in zip(drs, dcs):
         nr = r + dr
         nc = c + dc
-        
-        # 범위를 벗어나거나, 이미 방문했거나, 뱀이 있다면
-        if not in_range(nr, nc) or vistied[nr][nc] or not grid[nr][nc]:
+        if not in_range(nr, nc) or vistied[nr][nc] == 1 or grid[nr][nc] == 0:
             continue
-
         vistied[nr][nc] = 1
         move(nr, nc)
-
 
 move()
 print(vistied[N - 1][M - 1])
