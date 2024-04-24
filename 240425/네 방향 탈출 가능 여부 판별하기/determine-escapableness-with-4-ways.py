5cea3193265e1baa871959823e24657bbd76e96b
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-25 00:41:34 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 235ms, Memory: 26MB, Status: Passed - CodeTree

--- 240425/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240425/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,27 +1,28 @@
-from queue import Queue
+from collections import deque
 
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
-q = Queue()
 
 drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1]
 
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < m
 
+q = deque()
 def bfs():
-    q.put((0, 0))
+    
+    q.append((0, 0))
     visited[0][0] = True
 
-    while not q.empty():
-        r, c = q.get()
+    while q:
+        r, c = q.popleft()
         for dr, dc in zip(drs, dcs):
             nr, nc = r + dr, c + dc
             if in_range(nr, nc) and grid[nr][nc] == 1 and not visited[nr][nc]:
                 visited[nr][nc] = True
-                q.put((nr, nc))
+                q.append((nr, nc))
 
 bfs()
 print(1 if visited[n - 1][m - 1] else 0)
