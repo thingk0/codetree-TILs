3521834eac0ce46b7fae2da289ad7e3c8f6d0913
고명성 <75971566+thingk0@users.noreply.github.com>
2024-04-28 16:57:44 +0900

    Title: 이상한 폭탄, Time: 264ms, Memory: 29MB, Status: Passed - CodeTree

--- 240428/이상한 폭탄/strange-bomb.py
+++ 240428/이상한 폭탄/strange-bomb.py
@@ -1,21 +1,12 @@
 n, k = map(int, input().split())
-arr = [
-    int(input())
-    for _ in range(n)
-]
-r = [0] * n
 
 ldx = dict()
-for i in range(n - 1, -1, -1):
-    if arr[i] not in ldx:
-        r[i] = -1
-    else:
-        r[i] = ldx[arr[i]]
-    ldx[arr[i]] = i
-
 res = -1
+
 for i in range(n):
-    if r[i] != -1 and r[i] - i <= k:
-        res = max(res, arr[i])
+    t = int(input())
+    if t in ldx and i - ldx[t] <= k:
+        res = max(res, t)
+    ldx[t] = i
 
 print(res)
