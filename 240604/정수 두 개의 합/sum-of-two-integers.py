c2e12228262dc59f5a71b241f1817435417bcfea
고명성 <75971566+thingk0@users.noreply.github.com>
2024-06-04 21:01:40 +0900

    Title: 정수 두 개의 합, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240604/정수 두 개의 합/sum-of-two-integers.py
+++ 240604/정수 두 개의 합/sum-of-two-integers.py
@@ -1,16 +1,8 @@
 import sys
 
-std = sys.stdin
+m = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 
-n = int(std.readline())
-m = int(std.readline())
-
-cnt = 0
-nums = {}
-
-for num in map(int, list(std.readline().split())):
-    if (m - num) in nums:
-        cnt += nums.get(m - num)
-    nums[num] = nums.get(num, 0) + 1
-
+nums = set(map(int, sys.stdin.readline().split()))
+cnt = sum((m - num) in nums for num in nums)
 print(cnt)
