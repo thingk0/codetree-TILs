a7c24deee8e598a59ed1420e4de421b462df22f1
고명성 <75971566+thingk0@users.noreply.github.com>
2024-06-04 21:25:31 +0900

    Title: 연결된 그래프, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240604/연결된 그래프/connected-graph.py
+++ 240604/연결된 그래프/connected-graph.py
@@ -6,27 +6,26 @@ n, m = map(int, input().split())
 parent = [i for i in range(n + 1)]
 rank = [1] * (n + 1)
 
-def find(n):
-    if parent[n] != n:
-        parent[n] = find(parent[n])
-    return parent[n]
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
 
-def union(a, b):
-    root_a = find(a)
-    root_b = find(b)
+def union(x, y):
+    root_x = find(x)
+    root_y = find(y)
 
-    if root_a != root_b:
-        if rank[root_a] > rank[root_b]:
-            parent[root_b] = root_a
-        elif rank[root_b] < rank[root_a]:
-            parent[root_a] = root_b
+    if root_x != root_y:
+        if rank[root_x] > rank[root_y]:
+            parent[root_y] = root_x
+        elif rank[root_x] < rank[root_y]:
+            parent[root_x] = root_y
         else:
-            parent[root_b] = root_a
-            rank[root_a] += 1
+            parent[root_y] = root_x
+            rank[root_x] += 1
 
 for _ in range(m):
     a, b = map(int, input().split())
     union(a, b)
 
-
-print(parent.count(1) - 1)
+print(sum(1 for i in range(1, n + 1) if find(i) == 1) - 1)
