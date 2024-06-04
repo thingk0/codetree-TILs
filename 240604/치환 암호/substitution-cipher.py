73d8185086138ab881d4743310a764ab844db4ab
고명성 <75971566+thingk0@users.noreply.github.com>
2024-06-04 19:58:57 +0900

    Title: 치환 암호, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240604/치환 암호/substitution-cipher.py
+++ 240604/치환 암호/substitution-cipher.py
@@ -1,8 +1,6 @@
-# 암호화된 문자열 입력 받기
-encrypted_str = input()
+encrypted = input()
 
-# 변환 규칙 입력 받기
-substitution_rule = input()
-substitution_table = {cc: pc for cc, pc in zip(substitution_rule, map(chr, range(ord('a'), ord('z') + 1)))}
+rule = input()
+table = {ch:chr(idx + 97) for idx, ch in enumerate(rule)}
 
-print(''.join(substitution_table.get(ch, ' ') for ch in encrypted_str))
+print(''.join(table.get(ch, ' ') for ch in encrypted))
