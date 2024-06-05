1379b33bca279a84375d010f6f961e7703f79b74
고명성 <75971566+thingk0@users.noreply.github.com>
2024-06-05 23:38:28 +0900

    Title: DFS와 BFS, Time: 267ms, Memory: 29MB, Status: Passed - CodeTree

--- 240605/DFS와 BFS/DFS-and-BFS.py
+++ 240605/DFS와 BFS/DFS-and-BFS.py
@@ -3,16 +3,21 @@ import sys
 
 input = sys.stdin.readline
 
+# 노드 수, 간선 수, 시작 노드 입력
 n, m, s = map(int, input().split())
 
+# 그래프 초기화
 graph = {node: [] for node in range(1, n + 1)}
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
 
-for nodes in graph.values():
-    nodes.sort()
+# 간선 정보 입력
+for _ in range(m):
+    node_a, node_b = map(int, input().split())
+    graph[node_a].append(node_b)
+    graph[node_b].append(node_a)
+
+# 인접 리스트 정렬
+for adj_list in graph.values():
+    adj_list.sort()
 
 def dfs(cur, visited):
     if not visited[cur]:
@@ -21,15 +26,16 @@ def dfs(cur, visited):
         for next in graph[cur]:
             dfs(next, visited)
 
-def bfs(cur, visited):
-    q = deque([cur])
-    while q:
-        t = q.popleft()
-        if not visited[t]:
-            print(t, end=' ')
-            visited[t] = True
-            for add in graph[t]:
-                q.append(add)
+def bfs(start, visited):
+    queue = deque([start])
+    while queue:
+        cur = queue.popleft()
+        if not visited[cur]:
+            print(cur, end=' ')
+            visited[cur] = True
+            for next in graph[cur]:
+                queue.append(next)
+
 
 dfs(s, [False] * (n + 1))
 print()
