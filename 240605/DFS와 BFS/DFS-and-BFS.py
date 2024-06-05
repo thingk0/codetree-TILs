5b2cd8dde41738d779ff24f5a159cd6ca6df907d
고명성 <75971566+thingk0@users.noreply.github.com>
2024-06-05 23:31:55 +0900

    Title: DFS와 BFS, Time: 241ms, Memory: 29MB, Status: Passed - CodeTree

--- 240605/DFS와 BFS/DFS-and-BFS.py
+++ 240605/DFS와 BFS/DFS-and-BFS.py
@@ -5,29 +5,33 @@ input = sys.stdin.readline
 
 n, m, s = map(int, input().split())
 
-graph = {node: [] for node in range(1, n+1)}
+graph = {node: [] for node in range(1, n + 1)}
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(visited = [False] * (n+1), cur = 1):
+# 인접 리스트를 정렬하여 방문 순서를 정렬된 순서로 유지
+for nodes in graph.values():
+    nodes.sort()
+
+def dfs(cur, visited):
     if not visited[cur]:
         print(cur, end=' ')
         visited[cur] = True
         for next in graph[cur]:
-            dfs(visited, next)
+            dfs(next, visited)
 
-def bfs(visited = [False] * (n+1), cur = 1):
+def bfs(cur, visited):
     q = deque([cur])
     while q:
         t = q.popleft()
         if not visited[t]:
-            print(t, end = ' ')
+            print(t, end=' ')
             visited[t] = True
             for add in graph[t]:
                 q.append(add)
 
-dfs()
+dfs(s, [False] * (n + 1))
 print()
-bfs()
+bfs(s, [False] * (n + 1))
